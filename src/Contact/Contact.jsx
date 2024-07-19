0f8e1f5c74eaca2c09f27ec057b28637ba4698c2
Laiba Ahmed <laiba.sabahat9@gmail.com>
2024-07-19 15:27:44 +0500

    font changes

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,8 +1,4 @@
-import footerimg from "../media/footerimage.png";
 import "./Contact.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import CalendlySection from "./Calendly.jsx";
 
 const Contact = () => {
@@ -12,27 +8,7 @@ const Contact = () => {
         <h3>Let's Chat</h3>
         <p>I would love to learn about your project.</p>
         <CalendlySection className="Calendly" />
-        {/* <div className="contact-inner">
-          <p>
-            <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
-          </p>
-          <p>
-            <FontAwesomeIcon icon={faLinkedinIn} className="contact-icon" />
-          </p>
-           <button>
-            <a
-              href="https://calendly.com/laiba-sabahat9/15-min"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Book Meeting Now!
-            </a>
-          </button> 
-        </div> */}
       </div>
-      {/* <div className="contact-img">
-        <img src={footerimg} alt="footer-img" />
-      </div> */}
     </div>
   );
 };
